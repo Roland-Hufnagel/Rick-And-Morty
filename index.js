e8cad2765c67f9bf308262988da789ef334e5509
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-03-27 13:43:45 +0200

    add refactoring

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-import { createCharacterCard } from "./components/card/card.js";
+import createButton from "./components/nav-button/nav-button.js";
+import createPagination from "./components/nav-pagination/nav-pagination.js";
+import createCharacterCard from "./components/card/card.js";
+import createSearchBar from "./components/search-bar/search-bar.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+//const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
+//const prevButton = document.querySelector('[data-js="button-prev"]');
+//const nextButton = document.querySelector('[data-js="button-next"]');
+//const pagination = document.querySelector('[data-js="pagination"]');
+
+const prevButton = createButton("PREVIOUS", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
+  }
+});
+const nextButton = createButton("NEXT", () => {
+  if (page < maxPages) {
+    page++;
+    fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
+  }
+});
+const pagination = createPagination();
+navigation.append(prevButton, pagination, nextButton);
+
+const searchBar = createSearchBar(handleSubmit);
+searchBarContainer.append(searchBar);
 
 // States
 let maxPages = 1; // need to make it let
@@ -40,21 +61,27 @@ async function fetchCharacters(url) {
   pagination.textContent = `${page} / ${maxPages}`;
 }
 
-prevButton.addEventListener("click", () => {
-  if (page > 1) {
-    page--;
-    fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
-  }
-});
-nextButton.addEventListener("click", () => {
-  if (page < maxPages) {
-    page++;
-    fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
-  }
-});
-searchBar.addEventListener("submit", (event) => {
+// prevButton.addEventListener("click", () => {
+//   if (page > 1) {
+//     page--;
+//     fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
+//   }
+// });
+// nextButton.addEventListener("click", () => {
+//   if (page < maxPages) {
+//     page++;
+//     fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
+//   }
+// });
+// searchBar.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   searchQuery = event.target.elements.query.value;
+//   page = 1;
+//   fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
+// });
+function handleSubmit(event) {
   event.preventDefault();
   searchQuery = event.target.elements.query.value;
   page = 1;
   fetchCharacters(url + "?page=" + page + "&name=" + searchQuery);
-});
+}
